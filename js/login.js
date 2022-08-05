a9cc8efca18f082ecb681682f8320b845bdc8cec
mr-luckyyy8000 <108836409+mr-luckyyy8000@users.noreply.github.com>
2022-08-06 02:00:45 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,14 @@
 function validate(){
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    if(username=="123"&& password=="jaimahakal"){
+    if(username=="DOREMON"&& password=="JERRY"){
         // alert("login Successfully!!");
         window.open("index-educational.html");
+        return false;
+    }
+    if(username=="joker"&& password=="jaimahakal"){
+        // alert("login Successfully!!");
+        window.open("html1.html");
         return false;
     }
     else{
